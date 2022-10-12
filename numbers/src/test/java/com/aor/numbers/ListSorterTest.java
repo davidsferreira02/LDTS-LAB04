82ece95bb0bab788aad9e37aefb1a59fdcfe9f49
davidsferreira02 <davidsferreira02@gmail.com>
2022-10-12 19:06:53 +0100

    ERROR

--- numbers/src/test/java/com/aor/numbers/ListSorterTest.java
+++ numbers/src/test/java/com/aor/numbers/ListSorterTest.java
@@ -12,8 +12,8 @@ public class ListSorterTest {
     List<Integer> expected;
     @BeforeEach
     public void Helper(){
-        list = Arrays.asList(3, 2, 6, 1, 4, 5, 7);
-        expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        list = Arrays.asList(1,2,4,2,5);
+        expected = Arrays.asList(1,2,4,5);
     }
 
     @Test
@@ -25,5 +25,11 @@ public class ListSorterTest {
         List<Integer> sorted = sorter.sort(list);
 
         Assertions.assertEquals(expected, sorted);
+    }
+    @Test
+    public void bug_sort_8276() {
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(Arrays.asList(1, 2, 4, 2));
+        Assertions.assertEquals(Arrays.asList(1, 2, 2, 4), sorted);
     }
 }
