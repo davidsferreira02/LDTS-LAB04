75399ca87660995ac674179cfa618ea1b7141951
davidsferreira02 <davidsferreira02@gmail.com>
2022-11-01 15:21:43 +0000

    First Spock tests

--- numbers/src/test/groovy/FirstSpecification.groovy
+++ numbers/src/test/groovy/FirstSpecification.groovy
@@ -1,13 +1,46 @@
+import com.aor.numbers.DivisibleByFilter
 import com.aor.numbers.ListAggregator
 import com.aor.numbers.ListDeduplicator
 import com.aor.numbers.ListSorter
+import com.aor.numbers.PositiveFilter
 import spock.lang.Specification
 
+
+
+class DivisibleByFilterSpockTest extends Specification{
+
+    def 'Divisible 2'(){
+        given:
+        def filter=new DivisibleByFilter(2)
+        when:
+        def divisible =filter.accept(2)
+        then:
+        true==divisible
+
+        def notdivisible=filter.accept(3)
+        then:
+        false==notdivisible
+    }
+
+    def 'Divisible 3'(){
+        given:
+        def filter=new DivisibleByFilter(3)
+        when:
+        def divisible =filter.accept(3)
+        then:
+        true==divisible
+        def notdivisible =filter.accept(2)
+        then:
+        false==notdivisible
+    }
+
+
+}
 class ListAggregatorSpockTest extends Specification {
     private def list
 
     def setup() {
-        list = Arrays.asList(1,2,4,2,5)
+        list = Arrays.asList(1, 2, 4, 2, 5)
     }
 
     def 'Testing: sum'() {
@@ -66,19 +99,7 @@ class ListAggregatorSpockTest extends Specification {
         -1 == max
     }
 
-    def 'testing distinct bug 8726'() {
-        given:
-        def deduplicator = Mock(GenericListDeduplicator)
-        def aggregator = new ListAggregator()
-        deduplicator.deduplicate(_) >> Arrays.asList(1, 2, 4)
 
-        when:
-        def n_distinct = aggregator.distinct(Arrays.asList(1, 2, 4, 2), deduplicator)
-        System.out.println(n_distinct)
-
-        then:
-        3 == n_distinct
-    }
 
     def 'testing distinct bug 8726 with expectation'() {
         given:
@@ -91,5 +112,26 @@ class ListAggregatorSpockTest extends Specification {
         then:
         3 == n_distinct
         1 * deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2)) >> Arrays.asList(1, 2, 4)
+    }
+}
+
+class ListFiltererSpockTest extends Specification{
+
+}
+
+
+class PositiveFilterSpockTest extends Specification{
+
+
+
+    def 'Positive Filter'(){
+        given:
+        PositiveFilter filter=new PositiveFilter();
+        when:
+        def positive=filter.accept(2)
+        def negative=filter.accept(-1)
+        then:
+        true==positive
+        false==negative
     }
 }
